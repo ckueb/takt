22732220f8bfb9b7b9284bd3507af621198e22db
ckueb <c1k@chris-kuebler.de>
2025-12-25 13:39:31 +0100

    Update takt.js

--- netlify/functions/takt.js
+++ netlify/functions/takt.js
@@ -1,1 +1,16 @@
-placeholder
+export default async function handler() {
+  const hasKey = !!process.env.OPENAI_API_KEY;
+
+  return new Response(
+    JSON.stringify({
+      status: "ok",
+      openai_key_present: hasKey
+    }),
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  );
+}
